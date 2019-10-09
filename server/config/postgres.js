a172a116690c2bf66e64fed77bcaddb92b9553c7
Yongmin Kim <yongmin86k@gmail.com>
2019-10-09 09:41:30 -0700

    Create new postgress pool

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -1,15 +1,12 @@
 const { Pool } = require('pg');
 
 module.exports = (app) => {
-  /**
-   * @TODO: Configuration Variables
-   *
-   *  Retrieve the necessary information to connect to Postgres
-   *  For example: app.get('PG_DB')
-   */
   return new Pool({
-    /**
-     *  @TODO: Supply the correct configuration values to connect to postgres
-     */
+    user: app.get('PG_USER'),
+    host: app.get('PG_HOST'),
+    database: app.get('PG_DB'),
+    password: app.get('PG_PASSWORD'),
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
   });
 };
