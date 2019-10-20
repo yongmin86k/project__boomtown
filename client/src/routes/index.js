c77ba9064e3a729ec989ecedc6ee0487ca058b83
Yongmin Kim <yongmin86k@gmail.com>
2019-10-19 18:48:49 -0700

    Update router to display header when it's not the first page

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,29 +1,29 @@
 import React, { Fragment } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, withRouter } from "react-router-dom";
 import { MenuBar } from "../components";
 import { Home, Items, Profile, Share } from "../pages";
 
-export default () => (
-  <Fragment>
-    {/* @TODO: Add your menu component here */}
-    <MenuBar />
+const mainRoutes = ({ location }) => {
+  return (
+    <Fragment>
+      {location.pathname !== "/welcome" ? <MenuBar /> : ``}
 
-    <Switch>
-      <Route exact path="/welcome" component={Home} />
-      <Route path="/share" component={Share} />
-      <Route path="/items" component={Items} />
-      <Route path="/profile/:id" component={Profile} />
-      <Route path="/profile" component={Profile} />
-      <Redirect from="*" to="/items" />
+      <Switch>
+        <Route exact path="/welcome" component={Home} />
+        <Route path="/share" component={Share} />
+        <Route path="/items" component={Items} />
+        <Route path="/profile/:userid" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Redirect from="*" to="/items" />
 
-      {/**
-       * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
-       *
-       * Provide a wildcard redirect to /items for any undefined route using <Redirect />.
-       *
-       * Later, we'll add logic to send users to one set of routes if they're logged in,
-       * or only view the /welcome page if they are not.
-       */}
-    </Switch>
-  </Fragment>
-);
+        {/**
+         *
+         * Later, we'll add logic to send users to one set of routes if they're logged in,
+         * or only view the /welcome page if they are not.
+         */}
+      </Switch>
+    </Fragment>
+  );
+};
+
+export default withRouter(mainRoutes);
